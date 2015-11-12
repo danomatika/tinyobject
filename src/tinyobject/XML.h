deb8b685abe190489ea071f2af311124183a6794
danomatika <danomatika@gmail.com>
2015-11-12 07:51:24 -0700

    XML: fixed static initilization warnings; added README.md & data/test.xml to automake dist, renamed ChangeLog to standard naming for automake dist; moved premake obj dir to prj/obj; tagging version 0.4.3

--- src/tinyobject/XML.h
+++ src/tinyobject/XML.h
@@ -140,10 +140,15 @@ class XML {
 		/// "foo/bar/baz", "foo/1/bar/2/baz/3", etc
 		struct PathNode {
 			
-			std::string name = ""; //< element name
-			int index = 0; //< index if in a list, 0 for first element
+			std::string name; //< element name
+			int index; //< index if in a list, 0 for first element
 			
-			/// reset values
+			/// constructor to set default values
+			PathNode() {
+				clear();
+			}
+			
+			/// reset to default values
 			void clear() {
 				name = "";
 				index = 0;
